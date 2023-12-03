660c80bbba58b8ee6ec9c2c4dafbec4231ecf966
Antony Deepak Thomas <antonydeepak@gmail.com>
2023-12-02 22:58:35 -0800

    1

--- 1.cpp
+++ 1.cpp
@@ -13,7 +13,31 @@ int p1(const std::vector<std::string> &in) {
   return r;
 }
 
-long p2(const std::vector<std::string> &in) { return 0; }
+int p2(const std::vector<std::string> &in) {
+  constexpr std::array<const char *, 9> v{
+      "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  int r{0};
+  for (const auto &l : in) {
+    std::set<std::pair<size_t, std::string>> pos{};
+    for (size_t i{0}; i < v.size(); ++i) {
+      if (auto p = l.find(v[i]); p != l.npos) {
+        pos.emplace(p, std::to_string(i + 1));
+      }
+      if (auto p = l.rfind(v[i]); p != l.npos) {
+        pos.emplace(p, std::to_string(i + 1));
+      }
+    }
+    if (auto d = l.find_first_of("0123456789"); d != l.npos) {
+      pos.emplace(d, std::string{l[d]});
+    }
+    if (auto d = l.find_last_of("0123456789"); d != l.npos) {
+      pos.emplace(d, std::string{l[d]});
+    }
+    assert(pos.size() > 0);
+    r += std::stoi(pos.begin()->second + pos.rbegin()->second);
+  }
+  return r;
+}
 } // namespace
 
 int main() {
